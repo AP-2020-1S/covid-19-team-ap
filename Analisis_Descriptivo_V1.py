2d4591da0628be7c8bc7bdf79e1606440f954f99
tvergarac <68257810+tvergarac@users.noreply.github.com>
2020-08-21 19:31:00 -0500

    Script Download datos.gov
    funcion data con argumento lim que es la cantidad de datos a sacar de las bases de datos, recomendado 10 millones.
    
    devuelve los dataframes de pruebas, casos y uno de poblacion en ese orden

---
+++ Analisis_Descriptivo_V1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 19:29:32 2020
+
+@author: tomvc
+"""
+
+from download_data import data
+
+pruebas, casos, pob = data(1000)
---
+++ download_data.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 17:40:53 2020
+
+@author: tomvc
+"""
+
+import pandas as pd
+from sodapy import Socrata
+
+def data(lim):
+    
+    client = Socrata("www.datos.gov.co", None)
+    
+    pruebas_api = client.get("8835-5baf", limit=lim)
+    df_pruebas = pd.DataFrame.from_records(pruebas_api)
+    
+    del pruebas_api
+    
+    casos_api = client.get("gt2j-8ykr", limit=lim)
+    df_casos = pd.DataFrame.from_records(casos_api)
+
+    del casos_api
+    
+    pob_api = client.get("grgp-6bef", limit=lim)
+    df_pob = pd.DataFrame.from_records(pob_api)
+
+    del pob_api
+    
+    return df_pruebas, df_casos, df_pob
