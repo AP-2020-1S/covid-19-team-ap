1feb0da3fd8f1a1b36c81df1575368077a50d5d4
jpalacior <42697598+jpalacior@users.noreply.github.com>
2020-09-07 23:26:54 -0500

    actualizacion final

--- Dash/Actualizar App.bat
+++ Dash/Actualizar App.bat
@@ -1,5 +1,5 @@
-python "casos.py" & python "Actualizar modelos.py"
+call C:\Users\Julian\anaconda3\python.exe C:\Users\Julian\anaconda3\Scripts\activate casos.py
+call C:\Users\Julian\anaconda3\python.exe C:\Users\Julian\anaconda3\Scripts\activate "Actualizar modelos.py"
 call heroku container:login 
 call heroku container:push web -a teamap-unal 
 call heroku container:release web -a teamap-unal 
-call pause
