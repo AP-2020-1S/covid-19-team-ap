32425192d8937c20f835b84d82035f36c4c64541
jpalacior <42697598+jpalacior@users.noreply.github.com>
2020-09-07 22:58:53 -0500

    organizar

--- Dash/Actualizar App.bat
+++ Dash/Actualizar App.bat
@@ -1,4 +1,5 @@
+python "casos.py" & python "Actualizar modelos.py"
 call heroku container:login 
 call heroku container:push web -a teamap-unal 
 call heroku container:release web -a teamap-unal 
-& pause
+call pause
